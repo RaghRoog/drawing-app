88b6b933027bd24914a674b3cda8d937992b378f
RaghRoog <szumon612@interia.pl>
2024-07-22 12:21:45 +0200

    pencil drawing

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+let drawingArea = document.getElementById('drawingArea')
+let ctx = drawingArea.getContext('2d')
+
 //getting cords
 function getCords(event){
     return {
@@ -10,20 +13,37 @@ function getCords(event){
 let startingPoint = {}
 let endingPoint = {}
 
-drawingArea.addEventListener('mousedown', (event) => {
-    startingPoint = getCords(event)
-    console.log('starting point: ' + startingPoint.x + ", " + startingPoint.y)
-})
-drawingArea.addEventListener('mouseup', (event) => {
-    endingPoint = getCords(event)
-    drawingLine()
-    console.log('ending point: ' + endingPoint.x + ", " + endingPoint.y)
-})
 
 //drawing a line
-function drawingLine(){
+// function drawingLine(){
+//     ctx.beginPath()
+//     ctx.moveTo(startingPoint.x, startingPoint.y)
+//     ctx.lineTo(endingPoint.x, endingPoint.y)
+//     ctx.stroke()    
+// }
+// drawingArea.addEventListener('mousedown', (event) => {
+//     startingPoint = getCords(event)
+//     console.log('starting point: ' + startingPoint.x + ", " + startingPoint.y)
+// })
+// drawingArea.addEventListener('mouseup', (event) => {
+//     endingPoint = getCords(event)
+//     drawingLine()
+//     console.log('ending point: ' + endingPoint.x + ", " + endingPoint.y)
+// })
+
+//pencil
+function pencilDraw(event){
+    endingPoint = getCords(event)
     ctx.beginPath()
     ctx.moveTo(startingPoint.x, startingPoint.y)
     ctx.lineTo(endingPoint.x, endingPoint.y)
     ctx.stroke()    
+    startingPoint = endingPoint
 }
+drawingArea.addEventListener('mousedown', event => {
+    startingPoint = getCords(event)
+    drawingArea.addEventListener('mousemove', pencilDraw)
+})
+drawingArea.addEventListener('mouseup', event => {
+    drawingArea.removeEventListener('mousemove', pencilDraw)
+})
