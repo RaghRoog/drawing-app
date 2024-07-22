6fcfc4f513550ec4b3c66eb5e4d91d9ebacbec4e
RaghRoog <szumon612@interia.pl>
2024-07-22 14:19:51 +0200

    tool selection

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-let drawingArea = document.getElementById('drawingArea')
-let ctx = drawingArea.getContext('2d')
-
 //getting cords
 function getCords(event){
     return {
@@ -13,26 +10,15 @@ function getCords(event){
 let startingPoint = {}
 let endingPoint = {}
 
-
 //drawing a line
-// function drawingLine(){
-//     ctx.beginPath()
-//     ctx.moveTo(startingPoint.x, startingPoint.y)
-//     ctx.lineTo(endingPoint.x, endingPoint.y)
-//     ctx.stroke()    
-// }
-// drawingArea.addEventListener('mousedown', (event) => {
-//     startingPoint = getCords(event)
-//     console.log('starting point: ' + startingPoint.x + ", " + startingPoint.y)
-// })
-// drawingArea.addEventListener('mouseup', (event) => {
-//     endingPoint = getCords(event)
-//     drawingLine()
-//     console.log('ending point: ' + endingPoint.x + ", " + endingPoint.y)
-// })
-
-//pencil
-function pencilDraw(event){
+function drawingLine(ctx){
+    ctx.beginPath()
+    ctx.moveTo(startingPoint.x, startingPoint.y)
+    ctx.lineTo(endingPoint.x, endingPoint.y)
+    ctx.stroke()    
+}
+//drawing with pencil
+function pencilDraw(event, ctx){
     endingPoint = getCords(event)
     ctx.beginPath()
     ctx.moveTo(startingPoint.x, startingPoint.y)
@@ -40,10 +26,47 @@ function pencilDraw(event){
     ctx.stroke()    
     startingPoint = endingPoint
 }
-drawingArea.addEventListener('mousedown', event => {
-    startingPoint = getCords(event)
-    drawingArea.addEventListener('mousemove', pencilDraw)
-})
-drawingArea.addEventListener('mouseup', event => {
-    drawingArea.removeEventListener('mousemove', pencilDraw)
-})
+
+//removing event listeners from canvas
+function removeEventListenersFromCanvas(){
+    let drawingArea = document.getElementById('drawingArea')
+    let newDrawingArea = drawingArea.cloneNode(true)
+    newDrawingArea.getContext('2d').drawImage(drawingArea, 0, 0)
+    drawingArea.parentNode.replaceChild(newDrawingArea, drawingArea)
+    return newDrawingArea
+}
+
+//selecting drawing tool
+//line tool
+function selectLineTool(){
+    let drawingArea = removeEventListenersFromCanvas()
+    let ctx = drawingArea.getContext('2d')
+    drawingArea.addEventListener('mousedown', (event) => {
+        startingPoint = getCords(event)
+    })
+    drawingArea.addEventListener('mouseup', (event) => {
+        endingPoint = getCords(event)
+        drawingLine(ctx)
+   })
+}
+let selectLineBtn = document.getElementById('selectLineBtn')
+selectLineBtn.addEventListener('click', selectLineTool)
+//pencil tool
+function selectPencilTool(){
+    let drawingArea = removeEventListenersFromCanvas()
+    let ctx = drawingArea.getContext('2d')
+    let pencilDrawHandler
+    drawingArea.addEventListener('mousedown', event => {
+        startingPoint = getCords(event)
+        pencilDrawHandler = event => pencilDraw(event, ctx)
+        drawingArea.addEventListener('mousemove', pencilDrawHandler)
+    })
+    drawingArea.addEventListener('mouseup', () => {
+        drawingArea.removeEventListener('mousemove', pencilDrawHandler)
+    })
+    drawingArea.addEventListener('mouseleave', () => {
+        drawingArea.removeEventListener('mousemove', pencilDrawHandler)
+    })
+}
+let selectPencilBtn = document.getElementById('selectPencilBtn')
+selectPencilBtn.addEventListener('click', selectPencilTool)
